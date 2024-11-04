ad695cd811020bbb69456abb1d117face1fc8830
CornaeX <79000427+CornaeX@users.noreply.github.com>
2024-11-04 21:34:44 +0700

    Add files via upload

---
+++ api/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+app.get('/arduino', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+app.get('/ipst', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'IPST.html'));
+});
+app.get('/esp8266', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'ESP8266.html'));
+});
+app.get('/openkb', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'OPENKB.html'));
+});
+app.get('/creater', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'Creater.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
